bcd1c876e9e37ed20f548e72a8ba891c4e1a0c25
Mario Andres Botero Florez <mario.botero@cafexport.com>
2015-11-26 17:31:07 -0500

    start development in asp.net mvc

--- nAssembla.Web/Controllers/AccountAssemblaController.cs
+++ nAssembla.Web/Controllers/AccountAssemblaController.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using nAssembla.DTO;
 
 namespace nAssembla.Web.Controllers
 {
-    public class HomeController : Controller
+    public class AccountAssemblaController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult AssemblaAccount()
         {
 	  return View();
         }
 
-        public ActionResult Tutorial()
+        [HttpPost]
+        public ActionResult Autenticate(Authentication _autInfo)
         {
+	  string key = _autInfo.AccessToken;
+	  string d = _autInfo.
 	  return View();
         }
 
-        public ActionResult Configuration()
+        public ActionResult AccountConfiguration()
         {
 	  return View();
         }
